f219716b526980d5d02c0419b717f5d0c367f9a6
ptdv001 <peterdev001@hotmail.com>
2018-12-08 14:07:48 -0800

    Updates Home page design

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <h2>Welcome to the Chats</h2>
+  <div class="home m-4">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <div class="text-center mb-5">
+      <fa-icon :icon="['ptdv', 'home']" class="fa-5x" />
+      <h2>Welcome to the Chats</h2>
+    </div>
 
-    <p>Go to <router-link :to="{name: 'ChatApp', params: { id: 1 }}">Test Chat 1</router-link></p>
-    <p>Go to <router-link :to="{name: 'ChatApp', params: { id: 2 }}">Test Chat 2</router-link></p>
+    <h3>Current Chats</h3>
+    <ul class="chat-list">
+      <li>Go to <router-link :to="{name: 'ChatApp', params: { id: 1 }}">Test Chat 1</router-link></li>
+      <li>Go to <router-link :to="{name: 'ChatApp', params: { id: 2 }}">Test Chat 2</router-link></li>
+    </ul>
+
     <p>Go to Examples <router-link :to="{name: 'examples'}">Examples</router-link></p>
   </div>
 </template>
@@ -14,3 +21,7 @@ export default {
   name: "home"
 };
 </script>
+
+<style lang="scss" scoped>
+.home {}
+</style>
